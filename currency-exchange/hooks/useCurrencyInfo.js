144fb416ea07a265ec001d3b970ce50b2d08343e
Shayan Khan <shynkhn17@gmail.com>
2025-11-21 17:10:38 +0500

    fix the bugs in index.js

--- currency-exchange/hooks/useCurrencyInfo.js
+++ currency-exchange/hooks/useCurrencyInfo.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 function useCurrencyInfo(currency) {
     const [data , setdata] = useState({})
     useEffect(() =>{
-fetch(`https://api.exchangerate.host/latest?base=${currency}`)
+fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent('https://latest.currency-api.pages.dev/v1/currencies/usd.json')}`)
     
     .then((res)=>res.json())
     .then((res)=>setdata(res[currency]))
